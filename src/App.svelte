cf7f088c811e816b8580ac6c8b552836d2eed6fb
Lee Biddiscombe (Co-op IT - Retail) <lee.biddiscombe@coop.co.uk>
2020-09-24 21:36:33 +0100

    added identity

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,23 @@
 <script>
   import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs'
+  import * as netlifyIdentity from 'netlify-identity-widget'
+
   import Players from './components/Players.svelte'
   import Teams from './components/Teams.svelte'
+  import { onMount } from 'svelte'
+
+  let user
+
+  onMount(() => {
+    netlifyIdentity.init()
+    user = netlifyIdentity.currentUser()
+    netlifyIdentity.on('init', (user) => console.log('init', user))
+    netlifyIdentity.on('login', (user) => console.log('login', user))
+    netlifyIdentity.on('logout', () => console.log('Logged out'))
+    netlifyIdentity.on('error', (err) => console.error('Error', err))
+    netlifyIdentity.on('open', () => console.log('Widget opened'))
+    netlifyIdentity.on('close', () => console.log('Widget closed'))
+  })
 </script>
 
 <style>
@@ -25,18 +41,20 @@
 </style>
 
 <main>
-  <Tabs initialSelectedIndex={0}>
-    <TabList>
-      <Tab>Players</Tab>
-      <Tab>Teams</Tab>
-    </TabList>
+  {#if user}
+    <Tabs initialSelectedIndex={0}>
+      <TabList>
+        <Tab>Players</Tab>
+        <Tab>Teams</Tab>
+      </TabList>
 
-    <TabPanel>
-      <Players />
-    </TabPanel>
+      <TabPanel>
+        <Players />
+      </TabPanel>
 
-    <TabPanel>
-      <Teams />
-    </TabPanel>
-  </Tabs>
+      <TabPanel>
+        <Teams />
+      </TabPanel>
+    </Tabs>
+  {/if}
 </main>
